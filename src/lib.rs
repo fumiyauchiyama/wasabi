24c115ee87e09b459ac29fc9b2c48d8bf9e2dc5f
fumiyauchiyama <fumiyauchiyama.public@gmail.com>
2025-09-25 22:54:53 +0900

    added breakpoint

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![test_runner(crate::test_runner::test_runner)]
 #![reexport_test_harness_main = "run_unit_tests"]
 #![no_main]
+#![feature(offset_of)]
 pub mod allocator;
 pub mod graphics;
 pub mod init;
