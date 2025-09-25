09835bcaa102e023a20797fa1cf25697d2203be0
fumiyauchiyama <fumiyauchiyama.public@gmail.com>
2025-09-25 15:16:40 +0900

    implemented until hexdump and inspecting pagetable

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner::test_runner)]
+#![reexport_test_harness_main = "run_unit_tests"]
+#![no_main]
+pub mod allocator;
+pub mod graphics;
+pub mod init;
+pub mod print;
+pub mod qemu;
+pub mod result;
+pub mod serial;
+pub mod uefi;
+pub mod x86;
+
+#[cfg(test)]
+pub mod test_runner;
+
+#[cfg(test)]
+#[no_mangle]
+fn efi_main(image_handle: uefi::EfiHandle, efi_system_table: &uefi::EfiSystemTable) {
+    init::init_basic_runtime(image_handle, efi_system_table);
+    run_unit_tests()
+}
---
+++ src/x86.rs
@@ -0,0 +1,38 @@
+use core::arch::asm;
+
+pub fn hlt() {
+    unsafe { asm!("hlt") }
+}
+
+pub fn busy_loop_hint() {
+    unsafe { asm!("pause") }
+}
+
+pub fn read_io_port_u8(port: u16) -> u8 {
+    let mut data: u8;
+    unsafe {
+        asm!("in al, dx",
+            out("al") data,
+            in("dx") port)
+    }
+    data
+}
+
+pub fn write_io_port_u8(port: u16, data: u8) {
+    unsafe {
+        asm!("out dx, al",
+    in("al") data,
+    in("dx") port)
+    }
+}
+
+pub fn read_cr3() -> *mut RootPageTable {
+    let mut cr3: *mut RootPageTable;
+    unsafe {
+        asm!("mov rax, cr3",
+            out("rax") cr3)
+    }
+    cr3
+}
+
+pub type RootPageTable = [u8; 1024];
